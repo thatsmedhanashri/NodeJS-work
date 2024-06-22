2e75434b32c9f2dfb47d875d1bb2deea878cc346
thatsmedhanashri <dmp282021@gmail.com>
2024-06-22 13:32:32 +0530

    Created web server and written simple api

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -9,17 +9,46 @@ console.log("Input file text -> ", inputText)*/
 fs.writeFileSync('./txt/output.txt', outputText)*/
 
 // Reading file with callback function i.e. asnchronously
-fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
-    if(err){
-        return console.log("Error while reading start.txt!")
-    }else{
-        fs.readFile(`./txt/${data}.txt`, 'utf-8', (err, data1) => {
-            fs.readFile('./txt/append.txt', 'utf-8', (err, data2) => {
-                fs.writeFile('./txt/final.txt', `${data1}\n${data2}`, 'utf-8', (err) => {
-                    console.log("File written complete!")
-                })
-            })
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+//     if(err){
+//         return console.log("Error while reading start.txt!")
+//     }else{
+//         fs.readFile(`./txt/${data}.txt`, 'utf-8', (err, data1) => {
+//             fs.readFile('./txt/append.txt', 'utf-8', (err, data2) => {
+//                 fs.writeFile('./txt/final.txt', `${data1}\n${data2}`, 'utf-8', (err) => {
+//                     console.log("File written complete!")
+//                 })
+//             })
+//         })
+//     }
+// })
+// console.log("Reading file data....")
+
+// http module
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data)
+console.log("data - ", dataObj)
+
+const http = require('http')
+const server = http.createServer((req, res) => {
+    console.log("req - ", req.url)
+    if(req.url === '/' || req.url === '/overview'){
+        res.end("This is overview")
+    }else if(req.url === '/product'){
+        res.end("This is product info")
+    }else if(req.url === '/api'){
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
         })
+        res.end(data)
+    }else{
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'custom-header1': 'header1-value'
+        })
+        res.end("<h1>Page not found!</h1>")
     }
 })
-console.log("Reading file data....")
+server.listen('8000', '127.0.0.1', () => {
+    console.log("listening to port 8000")
+})
--- README.md
+++ README.md
@@ -5,3 +5,31 @@ But NodeJS doesn't execute in browser, it uses V8 engine to run the code.
 # Callback Hell
 Sometimes we need to execute functions one after the another because they are depending on previous functions. For this we need to write nested callback functions and it is called Callback Hell.
 Solution: Promises, Async/Await
+
+By default localhost's ip address is '127.0.0.1'
+
+## fs module and writing reading/writing files non-blocking way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+    if(err){
+        return console.log("Error while reading start.txt!")
+    }else{
+        fs.readFile(`./txt/${data}.txt`, 'utf-8', (err, data1) => {
+            fs.readFile('./txt/append.txt', 'utf-8', (err, data2) => {
+                fs.writeFile('./txt/final.txt', `${data1}\n${data2}`, 'utf-8', (err) => {
+                    console.log("File written complete!")
+                })
+            })
+        })
+    }
+})
+
+## http module and Setting status code and headers
+If we want to set headers and status code we need to set it before sending response content.
+res.writeHead(404, {
+    Content-type': 'text/html',
+    'custom-header1': 'header1-value'
+})
+res.end("<h1>Page not found!</h1>")
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+__dirname gives working directory name
